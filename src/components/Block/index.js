8e7af181aba65ded05f7661d63ed1e9fb7af53e6
unknown <allgloryforrobots@gmail.com>
2021-05-13 21:49:48 +0300

    'reinit'

--- src/components/Block/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Block.scss'
-import classNames from 'classnames'
-
-
-const Block = ({children, className}) => <div className={ classNames("block", className) }>{children}</div>
-
-
-export default Block
